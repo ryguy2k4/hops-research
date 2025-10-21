d09d8090c9fff6f45d71d402d132b6cbd02d2b17
Ryan Sponzilli <ryguy2k4@gmail.com>
2025-10-20 20:58:01 -0500

    stat tests

--- scripts/stat_test.py
+++ scripts/stat_test.py
@@ -61,44 +61,49 @@ def make_mixed_cumulates(pct_rand_list, include_obs=False):
 
 # generate cumulative distributions and p-values for simulated data
 data1, _ = make_mixed_cumulates(range(0, 101, 1), include_obs=False)
+data1['% orthogonal'] = 100 - data1['% random']
 plot_data, cumulates = make_mixed_cumulates([0, 25, 50, 75, 100], include_obs=True)
 
-# Generate cumulative plot
-fig, ax = plt.subplots(figsize=(11,8))
+# CUMULATIVE PLOT
+fig1, ax1 = plt.subplots(figsize=(11,8))
 # plot each distribution
 colors = ['#4477AA', '#AA3377', '#66CCEE', '#CCBB44', '#228833', '#EE6677']
 line_labels = ['Observations', '100% Orthogonal', '75% Orthogonal', '50% Orthogonal', '25% Orthogonal', 'Random']
 lines = []
 for i, item in enumerate(cumulates):
-    line, = ax.step(item[0], item[1], color=colors[i], linewidth=2.0)
+    line, = ax1.step(item[0], item[1], color=colors[i], linewidth=2.0)
     lines.append(line)
 # Plot Settings
-ax.set_title('$\\Delta$PA Cumulative Frequency Distribution', fontsize=28, pad=10)
-ax.set_xlabel('$\\Delta$PA - smallest angle between binary separation and outflow (degrees)', fontsize=20, labelpad=15)
-ax.set_ylabel('frequency', fontsize=20, labelpad=15)
-ax.set_xlim(0, 90.0)
-ax.set_ylim(0, 1.0)
-ax.set_xticklabels(ax.get_xticklabels(), fontsize=16)
-ax.set_yticklabels(ax.get_yticklabels(), fontsize=16)
+ax1.set_title('$\\Delta$PA Cumulative Frequency Distribution', fontsize=28, pad=10)
+ax1.set_xlabel('$\\Delta$PA - smallest angle between binary separation and outflow (degrees)', fontsize=20, labelpad=15)
+ax1.set_ylabel('frequency', fontsize=20, labelpad=15)
+ax1.set_xlim(0, 90.0)
+ax1.set_ylim(0, 1.0)
+ax1.set_xticklabels(ax1.get_xticklabels(), fontsize=16)
+ax1.set_yticklabels(ax1.get_yticklabels(), fontsize=16)
 labelLine(lines[0], label=line_labels[0], x=16, align=False, rotation=0, yoffset=-0.018, fontsize=14)
 labelLine(lines[1], label=line_labels[1], x=35, align=False, rotation=24, yoffset=0.03, fontsize=14)
 labelLine(lines[2], label=line_labels[2], x=40, align=False, rotation=28, yoffset=0.03, fontsize=14)
 labelLine(lines[3], label=line_labels[3], x=45, align=False, rotation=30, yoffset=0.035, fontsize=14)
 labelLine(lines[4], label=line_labels[4], x=50, align=False, rotation=32, yoffset=0.03, fontsize=14)
 labelLine(lines[5], label=line_labels[5], x=55, align=False, rotation=30, yoffset=0.03, fontsize=14)
-legend_labels = [f"p={round(p, 5)}" if (p != -1) else "Observations" for p in plot_data['p']]
-ax.legend(lines, legend_labels, fontsize=13)
-plt.savefig(os.path.join(output_folder, 'DeltaPA_cumulat_deg.pdf'), dpi=200)
+legend_labels = [f"p={round(p, 3)}" if (p != -1) else "Observations" for p in plot_data['p']]
+ax1.legend(lines, legend_labels, fontsize=13)
+fig1.savefig(os.path.join(output_folder, 'DeltaPA_cumulat_deg.pdf'), dpi=200)
 
-# p-value plot
+# P-VALUE PLOT
 fig2, ax2 = plt.subplots(figsize=(11,8))
-ax2.plot(100 - data1['% random'], data1['p'], color=colors[0])
-less_than_01 = 100 - data1.loc[data1['p']<0.1].iloc[0]['% random']
-less_than_005 = 100 - data1.loc[data1['p']<0.05].iloc[0]['% random']
-less_than_001 = 100 - data1.loc[data1['p']<0.01].iloc[0]['% random']
-plt.axhline(0.1, ls='dashed', label=f'% Orthogonal = {less_than_01}', color=colors[1])
-plt.axhline(0.05, ls='dashdot', label=f'% Orthogonal = {less_than_005}', color=colors[4])
-plt.axhline(0.01, ls='dotted', label=f'% Orthogonal = {less_than_001}', color=colors[5])
+ax2.plot(data1['% orthogonal'], data1['p'], color=colors[0])
+less_than_01 = data1.loc[data1['p']<0.1].iloc[0]['% orthogonal']
+less_than_005 = data1.loc[data1['p']<0.05].iloc[0]['% orthogonal']
+less_than_001 = data1.loc[data1['p']<0.01].iloc[0]['% orthogonal']
+peak = data1['% orthogonal'].iloc[data1['p'].argmax()]
+print(data1['p'].max())
+
+ax2.axvline(peak, ls='dotted', label=f"Orthogonal = {peak}% at maximum p-value", color='k')
+ax2.axvline(less_than_01, ls='dotted', label=f'Orthogonal = {int(less_than_01)}% at p=0.1', color=colors[1])
+ax2.axvline(less_than_005, ls='dotted', label=f'Orthogonal = {int(less_than_005)}% at p=0.05', color=colors[4])
+ax2.axvline(less_than_001, ls='dotted', label=f'Orthogonal = {int(less_than_001)}% at p=0.01', color=colors[5])
 
 ax2.set_title("% Orthogonal Outflows vs p-value", fontsize=28, pad=10)
 ax2.set_xlabel("% Orthogonal Outflows", fontsize=20, labelpad=15)
@@ -107,16 +112,16 @@ ax2.set_xlim(0,100)
 ax2.set_xticklabels(ax2.get_xticklabels(), fontsize=16)
 ax2.set_yticklabels(ax2.get_yticklabels(), fontsize=16)
 ax2.legend(fontsize=13)
-plt.savefig(os.path.join(output_folder, 'p-values.pdf'), dpi=200)
+fig2.savefig(os.path.join(output_folder, 'p-values.pdf'), dpi=200)
 
-# Delta PA Histrogram (nothing special)
-fig, ax = plt.subplots(figsize=(11,8))
-ax.hist(outflow_data['delta_PA'], label=f"N = {len(outflow_data[~outflow_data['delta_PA'].isna()])}", color='#1D58A7')
-ax.legend(loc='upper left', fontsize=32)
-ax.set_title("$\Delta$PA Distribution", fontsize=28, pad=10)
-ax.set_xlabel("$\Delta$PA - smallest angle between binary separation and outflow (degrees)", fontsize=20, labelpad=15)
-ax.set_ylabel("count", fontsize=20, labelpad=15)
+# DELTA PA HISTOGRAM
+fig3, ax3 = plt.subplots(figsize=(11,8))
+ax3.hist(outflow_data['delta_PA'], label=f"N = {len(outflow_data[~outflow_data['delta_PA'].isna()])}", color='#1D58A7')
+ax3.legend(loc='upper left', fontsize=32)
+ax3.set_title("$\Delta$PA Distribution", fontsize=28, pad=10)
+ax3.set_xlabel("$\Delta$PA - smallest angle between binary separation and outflow (degrees)", fontsize=20, labelpad=15)
+ax3.set_ylabel("count", fontsize=20, labelpad=15)
 
-ax.set_xticklabels(ax.get_xticklabels(), fontsize=16)
-ax.set_yticklabels(ax.get_yticklabels(), fontsize=16)
-fig.savefig(os.path.join(output_folder, "histogram.pdf"))
+ax3.set_xticklabels(ax3.get_xticklabels(), fontsize=16)
+ax3.set_yticklabels(ax3.get_yticklabels(), fontsize=16)
+fig3.savefig(os.path.join(output_folder, "histogram.pdf"))
